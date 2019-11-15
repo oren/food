134669ec7381543977e15d50be922ff21e272edc
Oren <orengolan@gmail.com>
2019-11-14 21:37:45 -0800

    add subject to the email

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -6,6 +6,4 @@
 <p>Not sure what setting I need (:</p>
 
 <h2>About</h2>
-<p>This is a simple and fast calorie counter app. If you have feature request contact me: <a href="mailto:orengolan@gmail.com">orengolan@gmail.com</a><p/>
-
-
+<p>This is a simple and fast calorie counter app. If you have feature request contact me: <a href="mailto:orengolan@gmail.com?subject=I love the calorie counter app!">orengolan@gmail.com</a><p/>
