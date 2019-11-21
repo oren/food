8862d9f7585de4d32bc8c1f9d458580abfe79942
Oren <orengolan@gmail.com>
2019-11-20 22:07:52 -0800

    style clear and update buttons

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ cp -r __sapper__/export/food/* ~/p/oren.github.io/food/
 ## Links
 * https://developers.google.com/web/tools/chrome-devtools/remote-debugging
 * https://codelabs.developers.google.com/codelabs/your-first-pwapp/#4
+* Look into purgeCSS (it removes all unused classes from any current page) since all the TailwindCSS classes are added to the global.css which ads kb's and ultimately a slower app.
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -173,8 +173,8 @@
 	<p><a href="/food/manage-food/add-food">Add some food first</a>.</p>
 {:else}
 
-	<button class='float-right bg-blue-500 text-white font-bold py-2 px-4 rounded' on:click={handleClear}>Clear</button>
-	<a class='inline-block float-right mt-3 rounded px-2 py-2 bg-blue-500 font-bold text-white' style="clear: both" href="/food/update">Update</a>
+	<button class='float-right max-w-md bg-blue-500 text-white font-bold py-2 px-4' on:click={handleClear}>Clear</button>
+	<a class='inline-block float-right max-w-md mt-1 px-2 py-2 bg-blue-500 font-bold text-white' style="clear: both" href="/food/update">Update</a>
 
 	{#if isGoal}
 	<div class='goal'>
