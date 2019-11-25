b53138cc1b75c7ba14fccca7389e7ef08498c35c
Oren <orengolan@gmail.com>
2019-11-24 16:53:24 -0800

    reverting the last commit

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,8 +17,8 @@
 	}
 </style>
 
-<nav class="bg-blue-200 h-10">
-	<ul class="flex min-h-full">
+<nav class="bg-blue-200">
+	<ul class="flex">
 		<li class="flex-1 mr-2">
 			<a class="{segment === undefined ? "selected" : ""} text-center block py-2 px-4" href="/food">Eat<Icon class="ml-2 mb-1" data={faUtensils}/></a>
 		</li>
