7409f68e266391f96edf6fbba094b72a7cccd57d
Oren <orengolan@gmail.com>
2019-11-29 16:26:16 -0800

    download icon

--- src/routes/manage-food/export.svelte
+++ src/routes/manage-food/export.svelte
@@ -36,8 +36,9 @@
 
 <h2 class="text-xl mb-1">Export Food</h2>
 
-<p class="mb-3">Exporting your food downloads a text file with all your food. You can save it somewhere as a backup or email it to a friend</p>
+<p class="mb-3">Exporting your food downloads a text file with all your food. You can save it somewhere as a backup or email it to a friend so they can imort it.</p>
 
-<button on:click={export2} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3">
-Export Food
+<button on:click={export2} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 inline-flex items-center">
+  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+  <span>Download</span>
 </button>
