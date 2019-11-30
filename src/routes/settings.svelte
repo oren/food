94d85a691d18c6cfc62b021b6e9299f7116201ed
Oren <orengolan@gmail.com>
2019-11-29 17:08:27 -0800

    new toggle button for goals

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -9,6 +9,65 @@
 .no-goal {
 	@apply bg-gray-100;
 }
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
 </style>
 
 <script>
@@ -50,16 +109,23 @@
 </script>
 
 <div>
+	<span class="mt-1 mr-2 float-left">Calories Goal:</span>
 	{#if isGoal}
-		Calories Goal:
-		<input type="checkbox" name="goal" checked value="yes" on:change={toggleGoal}>
+		<label class="switch float-left">
+			<input type="checkbox" checked on:change={toggleGoal}>
+			<span class="slider round"></span>
+		</label>
 	{:else}
-		Calories Goal:
-		<input type="checkbox" name="goal" value="no" on:change={toggleGoal}>
+		<label class="switch float-left">
+			<input type="checkbox" on:change={toggleGoal}>
+			<span class="slider round"></span>
+		</label>
 	{/if}
 
-	<input class={isGoal ? "goal" : "no-goal"} type="text" name="goal-text" bind:value={goal} on:keyup={goalChange} disabled={isGoal ? "" : "disabled"} maxlength="4" size="3">
+	<input class={isGoal ? "goal float-left mt-1 ml-2" : "no-goal float-left mt-1 ml-2"} type="text" name="goal-text" bind:value={goal} on:keyup={goalChange} disabled={isGoal ? "" : "disabled"} maxlength="4" size="3">
 </div>
+
+<div style="clear: both;"></div>
 
 <h2 class="text-xl mt-4">About</h2>
 <p>This is a simple and fast calorie counter app. If you have feature request contact me: <a href="mailto:orengolan@gmail.com?subject=I love the calorie counter app!">orengolan@gmail.com</a><p/>
