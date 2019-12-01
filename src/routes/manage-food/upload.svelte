2acfe7f307efcd6b1e55f65c3a1df1b9ef1b5d3c
Oren <orengolan@gmail.com>
2019-11-30 20:26:52 -0800

    basic feedback on upload file

--- src/routes/manage-food/upload.svelte
+++ src/routes/manage-food/upload.svelte
@@ -6,6 +6,7 @@
 	import Icon from 'svelte-awesome/components/Icon.svelte'
   import { upload } from 'svelte-awesome/icons';
 	let food = []
+	let done = false
 
 	function handleFile() {
 		const file = document.getElementById('input').files[0];
@@ -20,10 +21,17 @@
 					}
 				}
 
+				console.log('food', food)
+
 				localStorage.setItem('food', JSON.stringify(food))
 				food = food.slice(0,20)
 				localStorage.setItem('recentFood', JSON.stringify(food))
+				done = true
 			};
+
+		reader.onerror = function(event) {
+			console.error("File could not be read! Code " + event.target.error.code);
+		};
 
 		reader.readAsText(file);
 	}
@@ -34,6 +42,10 @@
 <p class="mb-3">The upload feature is in progress. Currently it deletes all your food and replace it with whatever you have in the text file. Also there is no validation so you might corrput your localStorage and if that's the case, you'll have to delete the website's storage to fix it</p>
 
 <form>
-	<input id="input" type="file" on:change={handleFile}>
+	 <label for="image_uploads">Choose file to upload (.txt)</label>
+	<input id="input" type="file" on:change={handleFile} accept=".txt">
 </form>
 
+{#if done}
+<p>File contain {food.length} food items</p>
+{/if}
