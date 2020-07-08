50787ae5110ebd40590c00e4c1b4e06f4b2f8199
Oren <orengolan@gmail.com>
2020-07-07 19:52:44 -0700

    width of edit count

--- src/routes/eat.svelte
+++ src/routes/eat.svelte
@@ -209,7 +209,7 @@
 		{#each foodIAte as { id, name, count }, i}
 			<button class="text-red-400 text-left" href="#" on:click|preventDefault={() => handleDelete(foodIAte[i])}><Icon data={trash}/></button>
 			<div>{name}</div>
-			<input class="bg-gray-200 w-10" type="text" on:keyup={(e) => handleEdit(foodIAte[i], e.target.value)} value={count} />
+			<input class="bg-gray-200 w-12" type="text" on:keyup={(e) => handleEdit(foodIAte[i], e.target.value)} value={count} />
 			<button class="" href="#" on:click|preventDefault={() => handlePlus(foodIAte[i])}><Icon data={plus}/></button>
 		{/each}
 	</div>
